962fd373a17d768dc9a9f7c686bc501747cd69e0
YiDaoJ <jcxsusan@gmail.com>
2022-10-21 18:02:57 +0200

    config for emotion in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,30 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../pages/**/*.stories.mdx",
     "../pages/**/*.stories.@(js|jsx|ts|tsx)",
     "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    // React preset + Emotion props
+    config.module.rules[0].use[0].options.presets = [
+      require.resolve("@babel/preset-react"),
+      require.resolve("@babel/preset-env"),
+      require.resolve("@emotion/babel-preset-css-prop"),
+    ];
+
+    return config;
+  },
+  features: {
+    emotionAlias: false,
+  },
+};
